300f830a88887a1da30b22935ccce14624ceae4d
Joao Loureiro <joao.loureiro@one.network>
2022-11-09 16:05:08 +0000

    Fix for touch events.

--- src/events.js
+++ src/events.js
@@ -1,13 +1,13 @@
-import setupModeHandler from './lib/mode_handler';
-import getFeaturesAndSetCursor from './lib/get_features_and_set_cursor';
-import featuresAt from './lib/features_at';
-import isClick from './lib/is_click';
-import isTap from './lib/is_tap';
-import * as Constants from './constants';
-import objectToMode from './modes/object_to_mode';
+import * as Constants from "./constants";
 
-export default function(ctx) {
+import featuresAt from "./lib/features_at";
+import getFeaturesAndSetCursor from "./lib/get_features_and_set_cursor";
+import isClick from "./lib/is_click";
+import isTap from "./lib/is_tap";
+import objectToMode from "./modes/object_to_mode";
+import setupModeHandler from "./lib/mode_handler";
 
+export default function (ctx) {
   const modes = Object.keys(ctx.options.modes).reduce((m, k) => {
     m[k] = objectToMode(ctx.options.modes[k]);
     return m;
@@ -19,11 +19,13 @@ export default function(ctx) {
   let currentModeName = null;
   let currentMode = null;
 
-  events.drag = function(event, isDrag) {
-    if (isDrag({
-      point: event.point,
-      time: new Date().getTime()
-    })) {
+  events.drag = function (event, isDrag) {
+    if (
+      isDrag({
+        point: event.point,
+        time: new Date().getTime(),
+      })
+    ) {
       ctx.ui.queueMapClasses({ mouse: Constants.cursors.DRAG });
       currentMode.drag(event);
     } else {
@@ -31,16 +33,19 @@ export default function(ctx) {
     }
   };
 
-  events.mousedrag = function(event) {
-    events.drag(event, endInfo => !isClick(mouseDownInfo, endInfo));
+  events.mousedrag = function (event) {
+    events.drag(event, (endInfo) => !isClick(mouseDownInfo, endInfo));
   };
 
-  events.touchdrag = function(event) {
-    events.drag(event, endInfo => !isTap(touchStartInfo, endInfo));
+  events.touchdrag = function (event) {
+    events.drag(event, (endInfo) => !isTap(touchStartInfo, endInfo));
   };
 
-  events.mousemove = function(event) {
-    const button = event.originalEvent.buttons !== undefined ? event.originalEvent.buttons : event.originalEvent.which;
+  events.mousemove = function (event) {
+    const button =
+      event.originalEvent.buttons !== undefined
+        ? event.originalEvent.buttons
+        : event.originalEvent.which;
     if (button === 1) {
       return events.mousedrag(event);
     }
@@ -49,52 +54,61 @@ export default function(ctx) {
     currentMode.mousemove(event);
   };
 
-  events.mousedown = function(event) {
+  events.mousedown = function (event) {
     mouseDownInfo = {
       time: new Date().getTime(),
-      point: event.point
+      point: event.point,
     };
     const target = getFeaturesAndSetCursor(event, ctx);
     event.featureTarget = target;
     currentMode.mousedown(event);
   };
 
-  events.mouseup = function(event) {
+  events.mouseup = function (event) {
     const target = getFeaturesAndSetCursor(event, ctx);
     event.featureTarget = target;
 
-    if (isClick(mouseDownInfo, {
-      point: event.point,
-      time: new Date().getTime()
-    })) {
+    if (
+      isClick(mouseDownInfo, {
+        point: event.point,
+        time: new Date().getTime(),
+      })
+    ) {
       currentMode.click(event);
     } else {
       currentMode.mouseup(event);
     }
   };
 
-  events.mouseout = function(event) {
+  events.mouseout = function (event) {
     currentMode.mouseout(event);
   };
 
-  events.touchstart = function(event) {
-    // Prevent emulated mouse events because we will fully handle the touch here.
-    // This does not stop the touch events from propogating to mapbox though.
-    event.originalEvent.preventDefault();
+  events.touchstart = function (event) {
     if (!ctx.options.touchEnabled) {
       return;
     }
 
     touchStartInfo = {
       time: new Date().getTime(),
-      point: event.point
+      point: event.point,
     };
     const target = featuresAt.touch(event, null, ctx)[0];
+
+    // If there are no mapbox targets nearby, let the event propagate through
+    if (!target && currentModeName === "simple_select") {
+      return;
+    }
+
+    // Prevent emulated mouse events because we will fully handle the touch here.
+    // This does not stop the touch events from propogating to mapbox though. (because of the lines above)
+    event.originalEvent.preventDefault();
+
     event.featureTarget = target;
     currentMode.touchstart(event);
   };
 
-  events.touchmove = function(event) {
+  events.touchmove = function (event) {
     event.originalEvent.preventDefault();
     if (!ctx.options.touchEnabled) {
       return;
@@ -104,18 +118,27 @@ export default function(ctx) {
     return events.touchdrag(event);
   };
 
-  events.touchend = function(event) {
-    event.originalEvent.preventDefault();
+  events.touchend = function (event) {
     if (!ctx.options.touchEnabled) {
       return;
     }
 
     const target = featuresAt.touch(event, null, ctx)[0];
+
+    // If there are no mapbox targets nearby, let the event propagate through
+    if (!target && currentModeName === "simple_select") {
+      return;
+    }
+
+    event.originalEvent.preventDefault();
+
     event.featureTarget = target;
-    if (isTap(touchStartInfo, {
-      time: new Date().getTime(),
-      point: event.point
-    })) {
+    if (
+      isTap(touchStartInfo, {
+        time: new Date().getTime(),
+        point: event.point,
+      })
+    ) {
       currentMode.tap(event);
     } else {
       currentMode.touchend(event);
@@ -124,12 +147,17 @@ export default function(ctx) {
 
   // 8 - Backspace
   // 46 - Delete
-  const isKeyModeValid = code => !(code === 8 || code === 46 || (code >= 48 && code <= 57));
+  const isKeyModeValid = (code) =>
+    !(code === 8 || code === 46 || (code >= 48 && code <= 57));
 
-  events.keydown = function(event) {
-    if ((event.srcElement || event.target).classList[0] !== 'mapboxgl-canvas') return; // we only handle events on the map
+  events.keydown = function (event) {
+    if ((event.srcElement || event.target).classList[0] !== "mapboxgl-canvas")
+      return; // we only handle events on the map
 
-    if ((event.keyCode === 8 || event.keyCode === 46) && ctx.options.controls.trash) {
+    if (
+      (event.keyCode === 8 || event.keyCode === 46) &&
+      ctx.options.controls.trash
+    ) {
       event.preventDefault();
       currentMode.trash();
     } else if (isKeyModeValid(event.keyCode)) {
@@ -143,20 +171,20 @@ export default function(ctx) {
     }
   };
 
-  events.keyup = function(event) {
+  events.keyup = function (event) {
     if (isKeyModeValid(event.keyCode)) {
       currentMode.keyup(event);
     }
   };
 
-  events.zoomend = function() {
+  events.zoomend = function () {
     ctx.store.changeZoom();
   };
 
-  events.data = function(event) {
-    if (event.dataType === 'style') {
+  events.data = function (event) {
+    if (event.dataType === "style") {
       const { setup, map, options, store } = ctx;
-      const hasLayers = options.styles.some(style => map.getLayer(style.id));
+      const hasLayers = options.styles.some((style) => map.getLayer(style.id));
       if (!hasLayers) {
         setup.addLayers();
         store.setDirty();
@@ -177,7 +205,7 @@ export default function(ctx) {
     currentMode = setupModeHandler(mode, ctx);
 
     if (!eventOptions.silent) {
-      ctx.map.fire(Constants.events.MODE_CHANGE, { mode: modename});
+      ctx.map.fire(Constants.events.MODE_CHANGE, { mode: modename });
     }
 
     ctx.store.setDirty();
@@ -187,17 +215,19 @@ export default function(ctx) {
   const actionState = {
     trash: false,
     combineFeatures: false,
-    uncombineFeatures: false
+    uncombineFeatures: false,
   };
 
   function actionable(actions) {
     let changed = false;
     Object.keys(actions).forEach((action) => {
-      if (actionState[action] === undefined) throw new Error('Invalid action type');
+      if (actionState[action] === undefined)
+        throw new Error("Invalid action type");
       if (actionState[action] !== actions[action]) changed = true;
       actionState[action] = actions[action];
     });
-    if (changed) ctx.map.fire(Constants.events.ACTIONABLE, { actions: actionState });
+    if (changed)
+      ctx.map.fire(Constants.events.ACTIONABLE, { actions: actionState });
   }
 
   const api = {
@@ -219,37 +249,37 @@ export default function(ctx) {
       }
     },
     addEventListeners() {
-      ctx.map.on('mousemove', events.mousemove);
-      ctx.map.on('mousedown', events.mousedown);
-      ctx.map.on('mouseup', events.mouseup);
-      ctx.map.on('data', events.data);
+      ctx.map.on("mousemove", events.mousemove);
+      ctx.map.on("mousedown", events.mousedown);
+      ctx.map.on("mouseup", events.mouseup);
+      ctx.map.on("data", events.data);
 
-      ctx.map.on('touchmove', events.touchmove);
-      ctx.map.on('touchstart', events.touchstart);
-      ctx.map.on('touchend', events.touchend);
+      ctx.map.on("touchmove", events.touchmove);
+      ctx.map.on("touchstart", events.touchstart);
+      ctx.map.on("touchend", events.touchend);
 
-      ctx.container.addEventListener('mouseout', events.mouseout);
+      ctx.container.addEventListener("mouseout", events.mouseout);
 
       if (ctx.options.keybindings) {
-        ctx.container.addEventListener('keydown', events.keydown);
-        ctx.container.addEventListener('keyup', events.keyup);
+        ctx.container.addEventListener("keydown", events.keydown);
+        ctx.container.addEventListener("keyup", events.keyup);
       }
     },
     removeEventListeners() {
-      ctx.map.off('mousemove', events.mousemove);
-      ctx.map.off('mousedown', events.mousedown);
-      ctx.map.off('mouseup', events.mouseup);
-      ctx.map.off('data', events.data);
+      ctx.map.off("mousemove", events.mousemove);
+      ctx.map.off("mousedown", events.mousedown);
+      ctx.map.off("mouseup", events.mouseup);
+      ctx.map.off("data", events.data);
 
-      ctx.map.off('touchmove', events.touchmove);
-      ctx.map.off('touchstart', events.touchstart);
-      ctx.map.off('touchend', events.touchend);
+      ctx.map.off("touchmove", events.touchmove);
+      ctx.map.off("touchstart", events.touchstart);
+      ctx.map.off("touchend", events.touchend);
 
-      ctx.container.removeEventListener('mouseout', events.mouseout);
+      ctx.container.removeEventListener("mouseout", events.mouseout);
 
       if (ctx.options.keybindings) {
-        ctx.container.removeEventListener('keydown', events.keydown);
-        ctx.container.removeEventListener('keyup', events.keyup);
+        ctx.container.removeEventListener("keydown", events.keydown);
+        ctx.container.removeEventListener("keyup", events.keyup);
       }
     },
     trash(options) {
@@ -263,7 +293,7 @@ export default function(ctx) {
     },
     getMode() {
       return currentModeName;
-    }
+    },
   };
 
   return api;
