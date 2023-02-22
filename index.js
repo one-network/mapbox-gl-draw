7d2ed14c1a7d62eaa29e4ae12391141b60ffab76
Joao Duarte <joao.duarte@one.network>
2023-02-22 11:26:12 +0000

    Adds lib and constants to the `MapboxDraw` namespace.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import runSetup from './src/setup';
-import setupOptions from './src/options';
-import setupAPI from './src/api';
-import * as Constants from './src/constants';
+import runSetup from "./src/setup";
+import setupOptions from "./src/options";
+import setupAPI from "./src/api";
+import * as Constants from "./src/constants";
+import * as lib from "./src/lib";
 
-const setupDraw = function(options, api) {
+const setupDraw = function (options, api) {
   options = setupOptions(options);
 
   const ctx = {
-    options
+    options,
   };
 
   api = setupAPI(ctx, api);
@@ -27,7 +28,9 @@ function MapboxDraw(options) {
   setupDraw(options, this);
 }
 
-import modes from './src/modes/index';
+import modes from "./src/modes/index";
 MapboxDraw.modes = modes;
+MapboxDraw.constants = Constants;
+MapboxDraw.lib = lib;
 
 export default MapboxDraw;
